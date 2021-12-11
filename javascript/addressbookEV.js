6efa4fb2aee19b5516bd509b7e04dfb36825ce8c
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-12-11 17:53:02 +0530

    UC6-Added

--- javascript/addressbookEV.js
+++ javascript/addressbookEV.js
@@ -84,3 +84,93 @@ const setTextValue = (id, value) => {
     const element = document.querySelector(id);
     element.textContent = value;
   };
+
+  const save = () => {
+	try	{
+		let addressBookData = createAddressBook();
+        createAndUpdateStorage(addressBookData); 
+    } catch (e) {
+		return;
+	}
+}
+
+function createAndUpdateStorage(addressBookData)
+{
+	let addressBookList = JSON.parse(localStorage.getItem("AddressBookList"));
+
+	if(addressBookList != undefined) {
+		addressBookList.push(addressBookData);
+	} else {
+		addressBookList = [addressBookData];
+	}
+	alert(addressBookList.toString());
+	localStorage.setItem("AddressBookList",JSON.stringify(addressBookList));
+}
+
+const createAddressBook = () => {
+	let addressBookData = new Contact();
+	try {
+		addressBookData.name = getInputValueById('#name');
+	} catch (e) {
+		setTextValue('.name-error', e);
+		throw e;
+	}
+	addressBookData.phoneNumber = getSelectedValues('[name=phoneNumber]').pop();
+	addressBookData.address = getSelectedValues('[name=address]').pop();
+    addressBookData.city = getSelectedValues('[name=city]').pop();
+    addressBookData.state = getSelectedValues('[name=state]').pop();
+    addressBookData.zip = getSelectedValues('[name=zip]').pop();
+	alert(addressBookData.toString());
+	return addressBookData;
+}
+
+const getSelectedValues = (propertyValue) => {
+	let allItems = document.querySelectorAll(propertyValue);
+	let selItems = [];
+	allItems.forEach(item => {
+		if(item.checked) selItems.push(item.value);
+	});
+	return selItems;
+}
+
+/*
+*	1: querySelector is the newer feature.
+*	2: The querySelector method can be used when selecting by element name,
+*		nestingm, or class name.
+*	3: querySelector lets you find elements with rules that can't be
+*		expressed with getElementById.
+*/
+const getInputValueById = (id) => {
+	let value = document.querySelector(id).value;
+	return value;
+}
+
+/*
+* 1: getElementById is better supported than querySelector in older versions
+*	of the browsers.
+* 2: The things with getElementById is that it only allows to select an
+*	element by its id.
+*/
+const getInputElementValue = (id) => {
+	let value = document.getElementById(id).value;
+	return value;
+}
+
+const resetForm = () => {
+	setTextValue('#name','');
+	setTextValue('#phoneNumber','');
+	setTextValue('#address','');
+	setValue('#city','');
+	setValue('#state','');
+    setTextValue('#zip','');
+}
+
+const setTextValue = (id, value) => {
+	const element = document.querySelector(id);
+	element.textContent = value;
+}
+
+const setValue = (id, value) => {
+	const element = document.querySelector(id);
+	element.value = value;
+}
